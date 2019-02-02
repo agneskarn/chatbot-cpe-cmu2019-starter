f5fd9eb020d6c85adb0ac1afe3e585a55e63d0f9
pakenham <pakenham98@gmail.com>
2019-02-02 16:06:53 +0700

    edit

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const restClient = new (require('node-rest-client').Client)
 
 const app = express()
 
+
 const config = {
-    channelAccessToken: 'XXX',
-    channelSecret: 'YYY'
-  }
+  channelAccessToken: 'HkGo35qA+7tEjFfF/5pAHxQ4laPCeohMs1XNtDNIekDQGdZU81VUy+cyaD3uP023XwZLldjUz6DNVckguBiHy/uB+gwXrhe4pbP9flUl0mBmoUF1kbj4IX7wjMKWSNF+AJFe7+kjeNQbyFwvqTX2aQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '0488d88a18a702e14c9d61baa0d1aa8e'
+}
 
 const client = new line.Client(config);
 
