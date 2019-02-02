6687e4ed12cb4b89005f7f8199857ebd64c402c9
pakenham <pakenham98@gmail.com>
2019-02-02 13:23:12 +0700

    add from cilent

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
+const Client = require('@line/bot-sdk').Client;
 const app = express()
+
+
 
 const config = {
   channelAccessToken: 'HkGo35qA+7tEjFfF/5pAHxQ4laPCeohMs1XNtDNIekDQGdZU81VUy+cyaD3uP023XwZLldjUz6DNVckguBiHy/uB+gwXrhe4pbP9flUl0mBmoUF1kbj4IX7wjMKWSNF+AJFe7+kjeNQbyFwvqTX2aQdB04t89/1O/w1cDnyilFU=',
   channelSecret: '0488d88a18a702e14c9d61baa0d1aa8e'
 }
 
+const Client = new Client(config)
+
 app.get('/', function (req, res) {
     res.send('Hello World!!')
+
 })
 
 app.post('/webhook', middleware(config), (req, res) => {
-  // req.body.events // webhook event objects
-  //req.body.destination // user ID of the bot (optional)
-  res.send('asadad')
+  res.send('asadad') // bot reply
+  const event = req.body.events[0]; // recieve msg
+
+  if (event.type === 'message') {
+    const message = event.message; // keep msg
+
+    client.replyMessage(event.replyToken, { // reply msg
+        type: 'text',
+        text: message, //ส่งอะไรมาส่งกลับมาแบบเดิม
+      });
+
+    }
 })
 
 app.set('port', (process.env.PORT || 4000))
