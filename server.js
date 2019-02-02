4ca60e5a215a8b7833b64e0f430da2f1fac4de2b
pakenham <pakenham98@gmail.com>
2019-02-02 11:17:26 +0700

    edit webhook

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
-
+const middleware = require('@line/bot-sdk').middleware
 const app = express()
+
+const config = {
+  channelAccessToken: 'HkGo35qA+7tEjFfF/5pAHxQ4laPCeohMs1XNtDNIekDQGdZU81VUy+cyaD3uP023XwZLldjUz6DNVckguBiHy/uB+gwXrhe4pbP9flUl0mBmoUF1kbj4IX7wjMKWSNF+AJFe7+kjeNQbyFwvqTX2aQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '0488d88a18a702e14c9d61baa0d1aa8e'
+}
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
+})
+
+app.post('/webhook', middleware(config), (req, res) => {
+  // req.body.events // webhook event objects
+  //req.body.destination // user ID of the bot (optional)
+  console.log('webhook success')
 })
 
 app.set('port', (process.env.PORT || 4000))
